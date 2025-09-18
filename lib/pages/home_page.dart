a9c2d1bf136d4330946975a1c14a4ed987c4cc12
dicenull <dicenull.dev@gmail.com>
2025-09-19 00:06:51 +0900

    support portrait

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+const primaryColor = Color(0xFF0c7bbb);
+
 class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
 
@@ -11,60 +13,72 @@ class HomePage extends HookConsumerWidget {
     final size = MediaQuery.sizeOf(context);
     const textColor = Color(0xFF333333);
 
+    final isPortrait = size.height >= size.width;
+    // 回転させて表示
+
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            child: _TriangleBanner(
-              size: Size(size.width * 0.1, size.width * 0.1),
+      body: RotatedBox(
+        quarterTurns: isPortrait ? 1 : 0,
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              child: _TriangleBanner(
+                size: Size(size.width * 0.1, size.width * 0.1),
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(size.width * 0.08),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '初星学園　プロデューサー科',
-                  style: textTheme.headlineSmall
-                      ?.copyWith(fontWeight: FontWeight.w600, color: textColor),
-                ),
-                const Gap(16),
-                SelectableText(
-                  'ダイス',
-                  style: textTheme.displayLarge
-                      ?.copyWith(fontWeight: FontWeight.bold, color: textColor),
-                ),
-                const Gap(4),
-                Text(
-                  '@dicenull',
-                  style: textTheme.titleMedium
-                      ?.copyWith(fontWeight: FontWeight.bold, color: textColor),
-                ),
-                const Spacer(),
-                const Row(
-                  children: [
-                    Icon(Icons.home_outlined, size: 16, color: textColor),
-                    Gap(2),
-                    Text('6VN36M29', style: TextStyle(color: textColor)),
-                  ],
-                ),
-                const Row(
-                  children: [
-                    Icon(Icons.person_outlined, size: 16, color: textColor),
-                    Gap(2),
-                    Text(
-                      '@dicenull',
-                      style: TextStyle(color: textColor),
+            Padding(
+              padding: EdgeInsets.all(size.width * 0.08),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '初星学園　プロデューサー科',
+                    style: textTheme.headlineSmall?.copyWith(
+                      fontWeight: FontWeight.w600,
+                      color: textColor,
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  const Gap(16),
+                  SelectableText(
+                    'ダイス',
+                    style: textTheme.displayLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: textColor,
+                    ),
+                  ),
+                  const Gap(4),
+                  Text(
+                    '@dicenull',
+                    style: textTheme.titleMedium?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: textColor,
+                    ),
+                  ),
+                  const Spacer(),
+                  const Row(
+                    children: [
+                      Icon(Icons.home_outlined, size: 16, color: textColor),
+                      Gap(2),
+                      Text('6VN36M29', style: TextStyle(color: textColor)),
+                    ],
+                  ),
+                  const Row(
+                    children: [
+                      Icon(Icons.person_outlined, size: 16, color: textColor),
+                      Gap(2),
+                      Text(
+                        '@dicenull',
+                        style: TextStyle(color: textColor),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -94,7 +108,7 @@ class _TrianglePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
-      ..color = const Color(0xFF0c7bbb)
+      ..color = primaryColor
       ..style = PaintingStyle.fill;
 
     final path = Path()
